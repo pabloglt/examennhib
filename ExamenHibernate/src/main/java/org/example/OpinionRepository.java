86da02a6e26b289f6a6af6d29e3527866158076c
pabloglt <pabloglt95@gmail.com>
2025-12-01 12:44:54 +0100

    Update OpinionRepository.java

--- ExamenHibernate/src/main/java/org/example/OpinionRepository.java
+++ ExamenHibernate/src/main/java/org/example/OpinionRepository.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
 import java.util.List;
 
 public class OpinionRepository {
@@ -27,7 +28,7 @@ public class OpinionRepository {
 
     public List<Opinion> findByUsuarioEmail(String email) {
         try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("FROM Opinion o WHERE o.usuarioEmail = :email", Opinion.class)
+            return session.createQuery("FROM Opinion o JOIN FETCH o.pelicula WHERE o.usuarioEmail = :email", Opinion.class)
                     .setParameter("email", email)
                     .list();
         }
@@ -35,8 +36,8 @@ public class OpinionRepository {
 
     public List<Opinion> findOpinionesPuntuacionBajo(int score) {
         try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("FROM Opinion o WHERE o.puntuacion <= :score", Opinion.class)
-                    .setParameter("puntuacion", score)
+            return session.createQuery("FROM Opinion o JOIN FETCH o.pelicula WHERE o.puntuacion <= :score", Opinion.class)
+                    .setParameter("score", score)
                     .list();
         }
     }
